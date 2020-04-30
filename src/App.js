d1ce35cfc021acd49e73322eb321447b090c0cad
Neeraj Kumar <neraj.thapa@gmail.com>
2020-05-01 04:57:55 +0530

    exact removed from reuter

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
      <BrowserRouter>
          <div>
              {/* <Header /> */}
-             <Route exact path="/" component={CarCard} />
+             <Route path="/" component={CarCard} />
              <Route exact path="/booking-form" component={FinalForm} />
              {/* <Route path="/surveys/new" component={Surveynew} /> */}
          </div>
